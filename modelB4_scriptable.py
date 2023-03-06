3f2d599005b795fee3c3e73adfa3523824e4abba
Dominik Vagala <d.vagala@gmail.com>
2023-03-06 16:56:22 +0100

    add 4 scriptable

--- modelB4_scriptable.py
+++ modelB4_scriptable.py
@@ -1,10 +1,8 @@
-# Lightweight Dense CNN for Edge Detection Just 3 blocks
-# It has less than 1Million parameters
+# Lightweight Dense CNN for Edge Detection
+# It has less than 1 Million parameters
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
-
 
 def weight_init(m):
     if isinstance(m, (nn.Conv2d,)):
@@ -24,20 +22,19 @@ def weight_init(m):
         if m.bias is not None:
             torch.nn.init.zeros_(m.bias)
 
-
 class CoFusion(nn.Module):
 
     def __init__(self, in_ch, out_ch):
         super(CoFusion, self).__init__()
         self.conv1 = nn.Conv2d(in_ch, 32, kernel_size=3,
                                stride=1, padding=1) # before 64
-        self.conv3 = nn.Conv2d(32, out_ch, kernel_size=3,
+        self.conv3= nn.Conv2d(32, out_ch, kernel_size=3,
                                stride=1, padding=1)# before 64  instead of 32
         self.relu = nn.ReLU()
-
         self.norm_layer1 = nn.GroupNorm(4, 32) # before 64
 
     def forward(self, x):
+        # fusecat = torch.cat(x, dim=1)
         attn = self.relu(self.norm_layer1(self.conv1(x)))
         attn = F.softmax(self.conv3(attn), dim=1)
         return ((x * attn).sum(1)).unsqueeze(1)
@@ -62,7 +59,6 @@ class _DenseLayer(nn.Module):
         new_features = self.fifth_layer(x)
 
         return 0.5 * (new_features + x2), x2
-
 
 
 class _DenseBlock2Layers(nn.Module):
@@ -120,7 +116,6 @@ class UpConvBlock(nn.Module):
     def forward(self, x):
         return self.features(x)
 
-
 class SingleConvBlock(nn.Module):
     def __init__(self, in_features, out_features, stride,
                  use_bs=True
@@ -136,7 +131,6 @@ class SingleConvBlock(nn.Module):
         if self.use_bn:
             x = self.bn(x)
         return x
-
 
 class DoubleConvBlock(nn.Module):
     def __init__(self, in_features, mid_features,
@@ -165,7 +159,6 @@ class DoubleConvBlock(nn.Module):
             x = self.relu(x)
         return x
 
-
 class LDC(nn.Module):
     """ Definition of the DXtrem network. """
 
@@ -173,21 +166,25 @@ class LDC(nn.Module):
         super(LDC, self).__init__()
         self.block_1 = DoubleConvBlock(3, 16, 16, stride=2,)
         self.block_2 = DoubleConvBlock(16, 32, use_act=False)
-        self.dblock_3 = _DenseBlock2Layers(32, 64) # [128,256,100,100]
+        self.dblock_3 = _DenseBlock2Layers(2, 32, 64) # [128,256,100,100]
+        self.dblock_4 = _DenseBlock3Layers(3, 64, 96)# 128
         self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
 
         # left skip connections, figure in Journal
         self.side_1 = SingleConvBlock(16, 32, 2)
+        self.side_2 = SingleConvBlock(32, 64, 2)
 
         # right skip connections, figure in Journal paper
+        self.pre_dense_2 = SingleConvBlock(32, 64, 2)
         self.pre_dense_3 = SingleConvBlock(32, 64, 1)
+        self.pre_dense_4 = SingleConvBlock(64, 96, 1)# 128
 
         # USNet
         self.up_block_1 = UpConvBlock(16, 1)
         self.up_block_2 = UpConvBlock(32, 1)
         self.up_block_3 = UpConvBlock(64, 2)
-
-        self.block_cat = CoFusion(3,3)# cats fusion method
+        self.up_block_4 = UpConvBlock(96, 3)# 128
+        self.block_cat = CoFusion(4,4)# cats fusion method
 
 
         self.apply(weight_init)
@@ -214,18 +211,27 @@ class LDC(nn.Module):
         block_2 = self.block_2(block_1) # 32 # [8,32,176,176]
         block_2_down = self.maxpool(block_2) # [8,32,88,88]
         block_2_add = block_2_down + block_1_side # [8,32,88,88]
-        # block_2_side = self.side_2(block_2_add) # [8,64,44,44] block 3 R connection
+        block_2_side = self.side_2(block_2_add) # [8,64,44,44] block 3 R connection
 
         # Block 3
         block_3_pre_dense = self.pre_dense_3(block_2_down) # [8,64,88,88] block 3 L connection
-        block_3, _ = self.dblock_3((block_2_add, block_3_pre_dense)) # [8,64,88,88]
+        block_3, _ = self.dblock_3([block_2_add, block_3_pre_dense]) # [8,64,88,88]
+        block_3_down = self.maxpool(block_3) # [8,64,44,44]
+        block_3_add = block_3_down + block_2_side # [8,64,44,44]
+
+        # Block 4
+        block_2_resize_half = self.pre_dense_2(block_2_down) # [8,64,44,44]
+        block_4_pre_dense = self.pre_dense_4(block_3_down+block_2_resize_half) # [8,96,44,44]
+        block_4, _ = self.dblock_4([block_3_add, block_4_pre_dense]) # [8,96,44,44]
+
 
         # upsampling blocks
         out_1 = self.up_block_1(block_1)
         out_2 = self.up_block_2(block_2)
         out_3 = self.up_block_3(block_3)
-
-        results = [out_1, out_2, out_3]
+        out_4 = self.up_block_4(block_4)
+        # results = [out_1, out_2, out_3, out_4, out_5, out_6]
+        results = [out_1, out_2, out_3, out_4]
 
         # concatenate multiscale outputs
         block_cat = torch.cat(results, dim=1)  # Bx6xHxW
